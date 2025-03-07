1f4e9be78360280928effae1fba54f5a48e56331
Sergey <sknyazev23@gmail.com>
2025-03-07 20:18:54 +0300

    initial commit

---
+++ src/main/java/com/github/javarashcommunity/jrtb/JavarashTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.javarashcommunity.jrtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarashTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarashTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/javarashcommunity/jrtb/JavarashTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javarashcommunity.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarashTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
